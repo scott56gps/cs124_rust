a9f8b4e94d7ec1d2a1611c2217d0a10f510da347
Scott Nicholes <scott93gps@gmail.com>
2022-07-25 23:52:35 -0700

    Initial Commit

---
+++ unit1/ex_1_0_first_program.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, I am Scott Nicholes.");
+    print!("\n");
+    print!("I love music.  One of my favorite things to do is to play the\n");
+    print!("piano and the organ.  I love finding new music, practicing it\n");
+    print!("and performing it for others.  I also love interacting with and\n");
+    println!("getting to know others.  I like working with others to help");
+    println!("them.");
+
+    println!("{}", format!("\tHola {:>8}!", "x"))
+}
