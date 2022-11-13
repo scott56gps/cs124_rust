b9ad6c9ef5a084c17379fb7876f0c496d60f22f2
Scott Nicholes <scott93gps@gmail.com>
2022-11-13 01:20:58 -0800

    Added 1.1 money alignment example

---
+++ unit1/ex1_1_money_align.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("${price1:>7}\t${price2:>7}", price1=124.45, price2=321.31);
+    println!("${price1:>7}\t${price2:>7}", price1=1.74, price2=4.21);
+    println!("${price1:>7}\t${price2:>7}", price1=7439.12, price2=54.92);
+}
